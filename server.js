917a35d7f762512774df199caab11adaf0efea93
jwechsler10 <steamacct2@gmail.com>
2018-01-29 14:33:59 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/test', (req, res) => res.send('Test'))
 
 app.listen(8080, '0.0.0.0')
 console.log("Server running on port 8080")
