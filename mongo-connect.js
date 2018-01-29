9f970e7fca5e511777f2aaab9e3ea8f3afdd3676
root <root@bastion-a176.oslab.opentlc.com>
2018-01-29 15:43:55 -0500

    trying with mongoose

--- mongo-connect.js
+++ mongo-connect.js
@@ -1,9 +1,12 @@
 // Retrieve
-let MongoClient = require('mongodb').MongoClient;
+var MongoClient = require('mongodb').MongoClient;
 
 // Connect to the db
 MongoClient.connect("mongodb://0.0.0.0:27017/exampleDb", function(err, db) {
   if(!err) {
     console.log("We are connected");
+  } 
+  else {
+    console.log(err); 
   }
 });
