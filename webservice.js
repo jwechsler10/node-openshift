601dea531decd83c688dd7f7280ad66fbc56e726
root <root@bastion-a176.oslab.opentlc.com>
2018-01-30 12:00:33 -0500

    added post for creating a cat

--- webservice.js
+++ webservice.js
@@ -4,13 +4,28 @@ var Kitten = schema.Kitten;
 
 const findByName = (req, res) => {
   var name = req.params.name
-  Kitten.find(name, (err, kitten) => {
+  Kitten.find({name: name}, (err, kitten) => {
     if (err) {
      res.status(400);
      return res.json(err);
   }
   return res.json(kitten);
 });
+
+}
+
+const createKitten = (req, res) => {
+  var name = req.body.name;
+  //var cat = new Kitten({ name: name});
+  Kitten.save({name: name}, (err, kitten) => {
+    if (err) {
+      res.status(400);
+      return res.json(err);
+
+  }
+    res.status(201);
+    return res.json(kitten._id);
+  });
 
 }
 
